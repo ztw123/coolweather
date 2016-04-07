23ff2d84a6b9acd4059470ab5876587b76444053
ztw123 <happyztw@sohu.com>
2016-04-08 00:04:56 +0800

    新增数据库帮助类，以及各表对应的实体类。

---
+++ src/com/coolweather/app/db/CoolWeatherOpenHelper.java
@@ -0,0 +1,50 @@
+package com.coolweather.app.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
+	
+	public CoolWeatherOpenHelper(Context context, String name,
+			CursorFactory factory, int version) {
+		super(context, name, factory, version);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		db.execSQL(CREATE_PROVINCE);
+		db.execSQL(CREATE_CITY);
+		db.execSQL(CREATE_COUNTY);
+	}
+	
+	/**
+	 * province 表建表语句
+	 */
+	public static final String CREATE_PROVINCE="create table Province("
+			+"id integer primary key autoincrement,"
+			+"province_name text,"
+			+"province_code text)";
+	/**
+	 * City表建表语句
+	 */
+	public static final String CREATE_CITY="create table City("
+			+"id integer primary key autoincrement,"
+			+"city_name text,"
+			+"city_code text,"
+			+"province_id integer)";
+	/**
+	 * County 表建表语句
+	 */
+	public static final String CREATE_COUNTY="create table County("
+			+ " id integer primary key autoincrement,"
+			+ "county_name text,"
+			+ "county_code text,"
+			+ "city_id integer)";
+}
